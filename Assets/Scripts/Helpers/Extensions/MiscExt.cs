b5c9b25873c1f7d47492fd8a002dfc16836e2e41
Ryan Chang <rchang0369@gmail.com>
2023-05-15 11:43:23 -0400

    Comment for Swap methods

--- Assets/Scripts/Helpers/Extensions/MiscExt.cs
+++ Assets/Scripts/Helpers/Extensions/MiscExt.cs
@@ -3,6 +3,13 @@
 /// </summary>
 public static class MiscExt
 {
+    /// <summary>
+    /// Swaps two values around. No performance benefits, just use if you are
+    /// lazy.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="a">To be assigned the value of b.</param>
+    /// <param name="b">To be assigned the value of a.</param>
     public static void Swap<T>(ref T a, ref T b)
     {
         T t = a;
