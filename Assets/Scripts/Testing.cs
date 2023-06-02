024433fcc520bdbb0b2096a1f93d634730058ba6
Ryan Chang <rchang0369@gmail.com>
2023-06-02 17:05:38 -0400

    ditto

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -58,7 +58,7 @@ public class Testing : MonoBehaviour
         sb.AppendTitle("Sample Text");
         sb.AppendLineAutoNewLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             prefix: "|=|", suffix: "|=|");
-        sb.AppendHeader("Sample Header");
+        sb.AppendH1("Sample Header");
         sb.AppendLineAutoNewLine("For example, the call to the String.Concat method in the following C# example appears to change the value of a string variable named value. In fact, the Concat method returns a value object that has a different value and address from the value object that was passed to the method. Note that the example must be compiled using the /unsafe compiler option.");
         // File.WriteAllText($"{Application.dataPath}/test.txt", sb.ToString());
 
