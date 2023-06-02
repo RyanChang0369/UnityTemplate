1ccaa5eae7f210f73d68adad8aa9ef5b51d09144
Ryan Chang <rchang0369@gmail.com>
2023-06-02 11:16:55 -0400

    String builder headers and title

--- Assets/Scripts/RyanChang/Helpers/Extensions/StringBuilderExt.cs
+++ Assets/Scripts/RyanChang/Helpers/Extensions/StringBuilderExt.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 /// <summary>
@@ -5,8 +7,139 @@ using System.Text;
 /// </summary>
 public static class StringBuilderExt
 {
-    public static void AppendSectionBreak(this StringBuilder sb)
+    /// <summary>
+    /// Appends a line to the string builder. Automatically breaks the line if
+    /// it goes over lineLen.
+    /// </summary>
+    /// <param name="sb">The string builder.</param>
+    /// <param name="line">The line of text to write.</param>
+    /// <param name="lineLen">The maximum length of the line before it is
+    /// broken, in characters.</param>
+    /// <param name="prefix">Optional prefix to add to the start of line and any
+    /// new lines created from breaking up the line.</param>
+    /// <param name="suffix">Optional prefix to add to the end of line and any
+    /// new lines created from breaking up the line.</param>
+    public static void AppendLineAutoNewLine(this StringBuilder sb,
+        string line, int lineLen = 80, string prefix = "", string suffix = "")
     {
-        sb.AppendLine("\r\n--------------------------------------------------------------------------------\r\n");
+        List<string> words = line.Split(" ").ToList();
+        int lineBodyLen = lineLen - prefix.Length - suffix.Length;
+
+        if (lineBodyLen < 1)
+            throw new System.ArgumentException("Cannot construct a line " +
+                "with length (suffix + prefix) " +
+                $"{suffix.Length + prefix.Length} and total line length of " +
+                $"{lineLen}.");
+
+        int wordsWritten = 0;
+        int c;
+
+        while (wordsWritten < words.Count)
+        {
+            // Write prefix.
+            sb.Append(prefix);
+
+            // Write characters until we are out of space for this line.
+
+            if (words[wordsWritten].Length > lineBodyLen)
+            {
+                // This word is too long. Split it.
+                string splitWordL = words[wordsWritten][0..lineBodyLen];
+                string splitWordR = words[wordsWritten][((byte)lineBodyLen)..];
+
+                words[wordsWritten] = splitWordL;
+                words.Insert(wordsWritten, splitWordR);
+            }
+
+            c = 0;
+            int nextC = words[wordsWritten].Length;
+
+            // Now write the word.
+            while (c + nextC <= lineBodyLen)
+            {
+                sb.Append(words[wordsWritten]);
+                c += words[wordsWritten].Length;
+                wordsWritten++;
+
+                if (wordsWritten < words.Count())
+                {
+                    // There is another word.
+                    nextC = words[wordsWritten].Length;
+
+                    if (c < lineBodyLen)
+                    {
+                        // Add the space.
+                        sb.Append(" ");
+                        c++;
+                    }
+                }
+                else
+                {
+                    // Wrote all the words.
+                    break;
+                }
+            }
+
+            // Write suffix (pad accordingly).
+            sb.AppendLine(suffix.PadLeft(lineLen - prefix.Length - c));
+        }
+    }
+
+    /// <summary>
+    /// Adds a vertical line to the string builder.
+    /// </summary>
+    /// <param name="sb">The string builder.</param>
+    /// <param name="lineLen">The length of the line.</param>
+    public static void AppendHorzLine(this StringBuilder sb, int lineLen = 80)
+    {
+        sb.AppendLine("".PadLeft(lineLen, '-'));
+    }
+
+    /// <summary>
+    /// Adds a section break to the string builder.
+    /// </summary>
+    /// <param name="sb">The string builder.</param>
+    /// <param name="lineLen">The length of the line.</param>
+    public static void AppendSectionBreak(this StringBuilder sb,
+        int lineLen = 80)
+    {
+        sb.AppendLine();
+        sb.AppendHorzLine(lineLen);
+        sb.AppendLine();
+    }
+
+    /// <summary>
+    /// Adds a formatted title to the string builder.
+    /// </summary>
+    /// <param name="sb">The string builder.</param>
+    /// <param name="title">The text of the title.</param>
+    /// <param name="lineLen">The length of the line.</param>
+    public static void AppendTitle(this StringBuilder sb, string title,
+        int lineLen = 80)
+    {
+        if (sb.Length > 0)
+            sb.AppendLine("\r\n");
+
+        sb.AppendHorzLine(lineLen);
+        sb.AppendLineAutoNewLine(title.ToUpper(), lineLen, "|    ", "    |");
+        sb.AppendHorzLine(lineLen);
+        sb.AppendLine();
+    }
+
+    /// <summary>
+    /// Adds a formatted header to the string builder.
+    /// </summary>
+    /// <param name="sb">The string builder.</param>
+    /// <param name="header">The text of the header.</param>
+    /// <param name="lineLen">The length of the line.</param>
+    public static void AppendHeader(this StringBuilder sb, string header,
+        int lineLen = 80)
+    {
+        if (sb.Length > 0)
+            sb.AppendLine("\r\n");
+
+        sb.AppendLineAutoNewLine(header, lineLen);
+        sb.AppendHorzLine(lineLen);
+        sb.AppendLine();
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
@@ -39,7 +41,7 @@ public class Testing : MonoBehaviour
     private void Start()
     {
         this.InstantiateSingleton(ref singleton);
-        
+
         test = new();
         test.Add(new(15, this, "Start"), new(17));
         test.Add(new(0, this, "Important"), new(0.5f, 0.5f));
@@ -51,5 +53,13 @@ public class Testing : MonoBehaviour
         {
             print(item.Key);
         }
+
+        StringBuilder sb = new();
+        sb.AppendTitle("Sample Text");
+        sb.AppendLineAutoNewLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            prefix: "|=|", suffix: "|=|");
+        sb.AppendHeader("Sample Header");
+        sb.AppendLineAutoNewLine("For example, the call to the String.Concat method in the following C# example appears to change the value of a string variable named value. In fact, the Concat method returns a value object that has a different value and address from the value object that was passed to the method. Note that the example must be compiled using the /unsafe compiler option.");
+        // File.WriteAllText($"{Application.dataPath}/test.txt", sb.ToString());
     }
 }
